d1e416d3b2f29253ba01dfd1c81d1b5fca8a569b
elif metin <elifxnur118@gmail.com>
2025-02-17 07:03:39 +0300

    day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_1/day_1/day1.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HorizontalListViewBuilder(),
+    ),
+  );
+}
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 1.5,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset('assets/img/avatars.jpg', fit: BoxFit.fill),
-              ),
-              Positioned(
-                top: 100,
-                left: 170,
-                child: Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text("Home Page")),
+      body: Center(child: Text("Welcome to Flutter")),
     );
   }
 }
